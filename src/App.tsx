b237c0491d5a2f65963a6cf3a9009c2df3f15d97
Wellington <wellington.map@hotmail.com>
2021-12-18 19:45:08 -0300

    create store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Provider } from 'react-redux'
+
+import store from './store'
+
 const App = function () {
-  return <h1>Hello World</h1>
+  return (
+    <Provider store={store}>
+      <h1>Hello World</h1>
+    </Provider>
+  )
 }
 
 export default App
