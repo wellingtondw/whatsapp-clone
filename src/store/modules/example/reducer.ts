fb12c85043c79073bdf738dc425049638a5c6342
Wellington <wellington.map@hotmail.com>
2021-12-18 19:53:52 -0300

    config reducer with types

--- src/store/modules/example/reducer.ts
+++ src/store/modules/example/reducer.ts
@@ -1,5 +1,12 @@
-function example() {
-  return []
+import { Reducer } from 'redux'
+import { IExampleState } from './types'
+
+const INITIAL_STATE: IExampleState = {
+  example: [],
+}
+
+const example: Reducer<IExampleState> = () => {
+  return INITIAL_STATE
 }
 
 export default example
