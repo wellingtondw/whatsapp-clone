24e0de2ac579495f4d3d60ccdccff379c8d4bbb9
Wellington <wellington.map@hotmail.com>
2021-12-19 20:33:10 -0300

    add redux devtools connection

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,6 @@
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 import { IExampleState } from './modules/example/types'
 
 import rootReducer from './modules/rootReducer'
@@ -7,6 +9,6 @@ export interface IState {
   example: IExampleState
 }
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools())
 
 export default store
